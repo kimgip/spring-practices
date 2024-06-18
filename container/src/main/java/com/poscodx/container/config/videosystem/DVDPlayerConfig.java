8d6aba5b69a0a0c47fb559973356b25d1c9398d9
kimgip <gippeum0102@gmail.com>
2024-06-18 10:54:21 +0900

    (Container) Bean Config Mixing(Import) Explicity -java + Spring Test

--- container/src/main/java/com/poscodx/container/config/videosystem/DVDPlayerConfig.java
+++ container/src/main/java/com/poscodx/container/config/videosystem/DVDPlayerConfig.java
@@ -1,11 +1,13 @@
 package com.poscodx.container.config.videosystem;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.poscodx.container.videosystem.Avengers;
 import com.poscodx.container.videosystem.DVDPlayer;
 import com.poscodx.container.videosystem.DigitalVideoDisc;
+import com.poscodx.container.videosystem.IronMan;
 
 @Configuration
 public class DVDPlayerConfig {
@@ -14,6 +16,11 @@ public class DVDPlayerConfig {
 		return new Avengers();
 	}
 
+	@Bean // 컨테이너가 빈 생성
+	public DigitalVideoDisc ironMan() {
+		return new IronMan();
+	}
+	
 	// 주입(Injection)하기 1
 	// Bean 생성 메소드를 직접 호출하는 방법
 	// 생성자 주입
@@ -26,7 +33,7 @@ public class DVDPlayerConfig {
 	// Parameter로 bean을 전달하는 방법
 	// 생성자 주입
 	@Bean
-	public DVDPlayer dvdPlayer2(DigitalVideoDisc dvd) {
+	public DVDPlayer dvdPlayer2(@Qualifier("avangers") DigitalVideoDisc dvd) {
 		return new DVDPlayer(dvd);
 	}
 	
@@ -34,7 +41,7 @@ public class DVDPlayerConfig {
 	// Parameter로 bean을 전달하는 방법
 	// setter 주입
 	@Bean("dvdPlayer03")
-	public DVDPlayer dvdPlayer3(DigitalVideoDisc dvd) {
+	public DVDPlayer dvdPlayer3(@Qualifier("avangers") DigitalVideoDisc dvd) {
 		DVDPlayer dvdPlayer = new DVDPlayer();
 		dvdPlayer.setDvd(dvd);
 		return dvdPlayer;
